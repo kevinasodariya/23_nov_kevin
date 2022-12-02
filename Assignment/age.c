9555eca184a365e467ab57991daff6a351307f8c
kevinasodariya <119500046+kevinasodariya@users.noreply.github.com>
2022-12-02 16:17:31 +0530

    Add files via upload

---
+++ Assignment/age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("value of no is:%d",a);
+	a++;
+	printf("\nyour next year is:%d",a);
+    a--;
+    a--;
+    printf("\nyour previous year age is:%d",a);
+}
---
+++ Assignment/birthdate.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("my birth-date is:29-8-2005");
+}
