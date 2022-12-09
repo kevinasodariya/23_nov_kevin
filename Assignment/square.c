74045a41e35c7c41ae823cd57126100ba20220f4
kevinasodariya <119500046+kevinasodariya@users.noreply.github.com>
2022-12-09 14:14:17 +0530

    Add files via upload

---
+++ Assignment/square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	printf("number \tsquare \tcube\n");
+	printf("=======================\n");
+	
+	int a;
+	for(a=0; a<=20; a++)
+	printf("%d\t%d\t%d\n",a,a*a,a*a*a);
+}
+	  
